552787ad18aedcf6822ba72cdcd4520d1c41750c
Artem <inste@icloud.com>
2019-05-23 11:20:16 +0300

    First commit

--- TableViewWithStoryboard/ViewController.swift
+++ TableViewWithStoryboard/ViewController.swift
@@ -9,12 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var numbers: [Int]!
 
+    @IBOutlet weak var myTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        generateNumbers()
+        
     }
 
-
+    private func generateNumbers() {
+        numbers = [Int]()
+        for _ in 0...1000 {
+            let number = Int.random(in: 10...20000)
+            numbers.append(number)
+        }
+    }
+    
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource, UITableViewDataSourcePrefetching {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numbers.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = String(numbers[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        print("prefetching row of \(indexPaths)")
+    }
+    
+    func tableView(_ tableView: UITableView, cancelPrefetchingForRowsAt indexPaths: [IndexPath]) {
+        print("cancel prefetch row of \(indexPaths)")
+    }
+}
